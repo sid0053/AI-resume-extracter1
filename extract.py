b938c36ac75b966c34342a523822ffa48ceac3ed
sid0053 <anut17780@gmail.com>
2025-11-05 01:12:43 +0530

    Added extract_resume_text function for integration

--- extract.py
+++ extract.py
@@ -42,3 +42,11 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+def extract_resume_text(file_path):
+    if file_path.endswith(".pdf"):
+        return extract_text_from_pdf(file_path)
+    elif file_path.endswith(".docx"):
+        return extract_text_from_docx(file_path)
+    else:
+        return ""
